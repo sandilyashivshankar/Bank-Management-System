4ca87bc80d66b66ff561cf34f7d9c040ae08fbba
Shiv shankar tiwari <154068823+sandilyashivshankar@users.noreply.github.com>
2026-06-20 15:27:38 +0530

    Update app.py

--- Bank-Management/app.py
+++ Bank-Management/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from hello import Bank
 
 st.set_page_config(page_title="Simple Bank App", layout="centered")
-st.title("🏦 Welcome to Streamlit Bank")
+st.title("🏦 Welcome to SS Bank")
 
 menu = st.sidebar.selectbox("Choose Action", ["Create Account", "Deposit", "Withdraw", "Show Details", "Update Info", "Delete Account"])
 
